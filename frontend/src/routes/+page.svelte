ddf65c4f9e6c8e70d83d302562096ed6afb84f79
m-guzik <mariagguzik@gmail.com>
2025-02-27 08:25:42 +0100

    Add basic FastAPI and SvelteKit setup

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+// @ts-nocheck
+
+
+  let apiData = [];
+
+    async function fetchData() {
+    const res = await fetch("http://127.0.0.1:8000/");
+    apiData = await res.json();
+    }
+
+  fetchData();
+</script>
+
+
+<h1>{apiData.message}</h1>
+<p>VisiBase - Visualiser for your Wikibase </p>
+
